ff0f81c39b7ff6eb180b5aa6902b8a4f75d11a47
Justas-Milisiunas <justas.milisiunas@gmail.com>
2020-11-02 10:58:44 +0200

    Fixed issue with point prefab position modifiying when instantiating

--- Assets/Scripts/Creators/PointCreator.cs
+++ Assets/Scripts/Creators/PointCreator.cs
@@ -8,13 +8,13 @@ public class PointCreator : MonoBehaviour
 
     public PointController CreatePoint(Point point)
     {
-        var newPointPrefab = pointPrefab.GetComponent<PointController>();
-        newPointPrefab.Order = point.OrderNumber;
+        var createdPoint = Instantiate(pointPrefab.GetComponent<PointController>());
+        createdPoint.Order = point.OrderNumber;
 
         float convertedX = (float)(point.Position.x * 0.018 - 9) * 0.9f;
         float convertedY = (float)(-(point.Position.y * 0.01 - 5));
 
-        newPointPrefab.transform.position = new Vector3(convertedX, convertedY, 0);
-        return Instantiate(newPointPrefab);
+        createdPoint.transform.position = new Vector3(convertedX, convertedY, 0);
+        return createdPoint;
     }
 }
